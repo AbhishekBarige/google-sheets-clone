173438a9e324567a059af5e1b386ddadd89b0cc1
AbhishekBarige <117386431+AbhishekBarige@users.noreply.github.com>
2025-01-13 20:52:21 +0530

    Delete frontend/test directory

--- frontend/test/test_formulas.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from services.formulas import sum_range, average
-
-def test_sum_range():
-    assert sum_range([1, 2, 3, 4, 5]) == 15
-
-def test_average():
-    assert average([1, 2, 3, 4, 5]) == 3.0
